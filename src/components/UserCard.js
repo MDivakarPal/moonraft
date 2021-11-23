7fa18a0a10ab33dc24727e10c81c148a1a37b060
Divakar Pal <31034949+MDivakarPal@users.noreply.github.com>
2021-11-23 16:39:07 +0530

    Update UserCard.js

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,33 +5,37 @@ function UserCard({ id, name, email, gender, joiningDate, location, deptt, about
     const [showAbout,setShowAbout]=useState(about.length>100 ? about.slice(0,100):about);
     const [left,setLeft]=useState(about.length>100 ? true :false);
     return (
-        <Container>
+        <Container fluid>
             <Row>
-            <Col>
-                <Col>
+            <Col md={6}>
+                <Row>
+                <Col className='text-end'>
                     <h6>Id:</h6>
                     <h6>Name:</h6>
                     <h6>Email:</h6>
                     <h6>Gender:</h6>
                 </Col>
-                <Col>
+                <Col className='text-start'>
                     <h6>{id}</h6>
                     <h6>{name}</h6>
                     <h6>{email}</h6>
                     <h6>{gender}</h6>
                 </Col>
+                </Row>
             </Col>
-            <Col>
-                <div>
+            <Col md={6}>
+               <Row>
+                <Col className='text-end'>
                     <h6>Joining Date :</h6>
                     <h6>Location:</h6>
                     <h6>Department:</h6>
-                </div>
-                <div>
+                </Col>
+                <Col className='text-start'>
                     <h6>{joiningDate}</h6>
                     <h6>{location}</h6>
                     <h6>{deptt}</h6>
-                </div>
+                </Col>
+                </Row>
             </Col>
             </Row>
             <Row>
